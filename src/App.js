41aae056590999f9e8280882205aae03cf055695
Dvontre Coleman <dvontrec@gmail.com>
2019-05-07 10:49:49 -0400

    change to semistandard (with semicolons)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class App extends Component {
   render () {
-    return <div className='App'>{/*  Say Hello World Here */}</div>
+    return <div className='App'>{/*  Say Hello World Here */}</div>;
   }
 }
 
-export default App
+export default App;
