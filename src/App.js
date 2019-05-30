6808ef3fa0d4baaba20eacd681165ee24e53b615
Justin Schuhmann <jmschu02@gmail.com>
2019-05-30 19:40:03 -0400

    added delete function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor () {
     super();
     this.state = {
-      greeting: 'We did it!',
+      nameInput: '',
+      ageInput: '',
       people: [
         { name: 'Alex', age: 21 },
         { name: 'Ben', age: 18 },
@@ -14,17 +15,56 @@ class App extends Component {
     };
   }
 
+  deletePerson (index) {
+    const { people } = this.state;
+    people.splice(index, 1);
+    this.setState({ people });
+  }
+
   // Class based method to render each person's name and age
   getPeople () {
     let people = this.state.people;
-    return people.map(function (person) {
+    let that = this;
+    return people.map(function (person, index) {
       return (
-        <div key={person.name}>
-          <h2>{person.name}</h2>
+        <div key={index}>
+          <h2>{person.name} <div onClick={() => { that.deletePerson(index) }}>delete</div></h2>
           <p>{person.age}</p>
         </div>
       );
     });
+  }
+
+  addPerson = (event) => {
+    event.preventDefault();
+    const { people } = this.state;
+    // const people = this.state.people;
+
+    const person = {
+      name: this.state.nameInput,
+      age: this.state.ageInput
+    };
+
+    people.push(person);
+    this.setState({ people, nameInput: '', ageInput: '' })
+  }
+
+  renderForm () {
+    return (
+      <form onSubmit={this.addPerson}>
+        <label>Name: </label>
+        <input type="text" id="name" value={this.state.nameInput} onChange={(event) => {
+          this.setState({ nameInput: event.target.value});
+          }}
+        />
+        <label>Age: </label>
+        <input type="text" id="age" value={this.state.ageInput} onChange={(event) => {
+          this.setState({ ageInput: event.target.value});
+          }}
+        />
+        <input type="submit"/>
+      </form>
+    )
   }
 
   // Method to render a div countaining the name and age of the youngest person in the state's people array
@@ -45,7 +85,12 @@ class App extends Component {
   }
 
   render () {
-    return <div>{this.getPeople()}</div>;
+    return <div>
+      <h1>{this.props.greeting}</h1>
+      <h2>Add Person: </h2>
+      <div>{this.renderForm()}</div>
+      <div>{this.getPeople()}</div>
+    </div>
   }
 }
 
